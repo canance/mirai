fa0c5d3deee942a49054dd875981c1bff3d747e7
randomdsu <randomdsu@users.noreply.github.com>
2017-04-02 18:41:10 -0400

    add banner mod

--- antimirai.py
+++ antimirai.py
@@ -46,19 +46,20 @@ def get_args():
         description='Script implements various protections against Mirai botnet ability to compromise.',
         formatter_class=argparse.RawTextHelpFormatter)
     # Add arguments
+    parser.add_argument('-q', '--quiet', help='Do not ask for confimations', action='store_true')
     parser.add_argument('-t', '--target', type=str, help='* Target device name/ip', required=True)
     parser.add_argument('-p', '--port', type=int, help='* Port number', required=True)
     parser.add_argument('-u', '--user', type=str, help='* Logon user name', required=True)
     parser.add_argument('-pw', '--password', type=str, help='* Password for --user', required=True)
-    parser.add_argument('-f', '--file_exec', type=str, help='Execute file on device, supply filname')
+    parser.add_argument('-f', '--file_exec', type=str, help='Execute file on device, supply filename')
     parser.add_argument('-o', '--output', type=str, help='Output file (default "date_time_mirai_harden_TARGET_PORT")e',
                         required=False, nargs='+', default=DATETIME)
     parser.add_argument('-s', '--severity', type=int,
                         help='''Hardening/Severity level, larger number takes more actions that are cummulative:
     1 - change default pass (csv write "date_time_mirai_changePW_IP|FQDN" in same format as (-f) target file)
-    2 - implement host.deny tcp/23 and/or tcp/22 for all but local LAN (
+    2 - change banner/motd/issue.net file.
+    3 - implement host.deny tcp/23 and/or tcp/22 for all but local LAN (
         only works if a daemon is compiled with tcp wrappers, ldd /usr/sbin/sshd | grep 'libwrap')
-    3 - iptables rules with drop and logging
     4 - change default port for telnet/ssh to random in /etc/services or restart busybox telnetd on diff port
     5 - create new random user, disable/nologin vulnerable one
     6 - fake vuln => chroot jail telnet, log everything
@@ -73,15 +74,15 @@ def get_args():
 
     # Array for all arguments passed to script
     args = parser.parse_args()
-    logging.info(args)
+
     # Return all variable values
     print(args)
-    return args.target, args.port, args.user, args.password, args.file_exec, args.output, args.severity, ' '.join(
+    return args.quiet, args.target, args.port, args.user, args.password, args.file_exec, args.output, args.severity, ' '.join(
         args.remote_arg), args.proto
 
 
 # create random alpha-numeric password
-def passwd_gen(size=12, chars=string.ascii_uppercase + string.digits):
+def random_gen(size=12, chars=string.ascii_uppercase + string.digits):
     return ''.join(random.choice(chars) for _ in range(size))
 
 
@@ -97,7 +98,7 @@ def login_telnet(tn, user, password):
 
 
 def change_passwd_telnet(tn):
-    # p = passwd_gen()
+    # p = random_gen()
     p = 'admin'
     tn.write("passwd " + user + "\n")
     tn.read_until("(current) UNIX password: ")
@@ -128,7 +129,7 @@ def is_open(ip, port):
         sockname = s.getsockname()[0]
         return sockname, True
     except:
-        log.error("%s:%d is not open" % (ip, port))
+        log.warning("%s:%d is not open" % (ip, port))
         return False
 
 
@@ -226,9 +227,57 @@ def split_by_length(s, block_size):
     return w
 
 
+def change_banner():
+    tn.read_until(CMD_PROMPT, 1)
+    #cp /etc/issue.orig /etc/issue ; cp /etc/issue.net.orig /etc/issue.net; cp /etc/motd.orig /etc/motd; rm -rf /etc/*20170*
+    tn.write('cp /etc/issue /etc/issue.' + DATETIME + ' && echo "' + random_gen(20) + '" > /etc/issue \n')
+    tn.write('cp /etc/issue.net /etc/issue.net.' + DATETIME + ' && echo "' + random_gen(20) + '" > /etc/issue.net \n')
+    tn.write('cp /etc/motd /etc/motd.' + DATETIME + ' && echo "' + random_gen(1000) + '" > /etc/motd \n')
+    print tn.read_until(CMD_PROMPT, 3)
+
+
+def start_telnet():
+    if (quiet or (query_yes_no("Are you sure you want to restart telnet on a different port? "
+                               "(currently %d): " % port, default="no"))):
+        sys.stdout.write("What port do you want to listen on? : ")
+        new_port = raw_input()
+        if is_open(target, int(new_port)):
+            log.error("TCP/%s appears to be in use!" % new_port)
+        else:
+            if int(new_port) >= 1 and int(new_port) <= 65535:
+                # get pid of current listening process we orginally connected to (eg TCP/23)
+                tn.read_until(CMD_PROMPT, 1)
+                tn.write("netstat -tlpn |grep :" + str(port) + "|awk -F \/ '{print $1}'|awk '{print $7}' > "
+                                                               "/tmp/tpid.out\n")
+                tn.read_until(CMD_PROMPT, 1)
+
+                # get process parms to re-run telnet on diff port
+                tn.write("ps -hf -o cmd -p $(cat /tmp/tpid.out) > /tmp/bb_location.out \n")
+                tn.read_until(CMD_PROMPT, 1)  # get busybox telnet parms
+                tn.write("nohup $(cat /tmp/bb_location.out)  -p " + str(
+                    new_port) + " >/dev/null 2>&1 & \n")  # " >/dev/null 2>&1 &"
+                print tn.read_until(CMD_PROMPT, 1)
+                if is_open(target, int(new_port)):
+                    log.info("TCP/%s is now open." % new_port)
+                    if (quiet or (query_yes_no("Ready to kill telnet on %s? " % port, default="no"))):
+                        kill_telnet(tn, str(port))
+            else:
+                print("Port selection out of range!")
+
+
+def create_new_user(tn, src_user):
+   pass
+
+
+def kill_telnet(tn, port):
+    tn.read_until(CMD_PROMPT, 1)
+    tn.write("kill -9 $(netstat -tlpn |grep :" + str(port) + "|awk -F \/ '{print $1}'|awk '{print $7}') \n")
+    print tn.read_until(CMD_PROMPT, 1)
+
+
 if __name__ == '__main__':
     # Run get_args()
-    target, port, user, password, file_exec, output, severity, arg_str, proto = get_args()
+    quiet, target, port, user, password, file_exec, output, severity, arg_str, proto = get_args()
     targetDetails = "%s:%d:%s:%s:%s" % (target, port, proto, user, password)
 
     # setup logging
@@ -257,34 +306,29 @@ if __name__ == '__main__':
                 change_passwd_telnet(tn)
                 break
             if case(2):
+                log.info("Changing banner/motd/issue.net...")
+                change_banner()
+                break
+            if case(3):
                 log.info("Writing hosts file option...")
                 write_hosts_file(sockname)
                 break
             if case(4):
                 log.info("Change listening port option.")
-                if (query_yes_no("Are you sure you want to restart telnet on a different port? (currently %d): " % port, default="no")):
-                    sys.stdout.write("What port do you want to listen on? : ")
-                    new_port = raw_input().isdigit()
-                    if new_port >= 1 and new_port <= 65535:
-                        tn.read_until(CMD_PROMPT, 1)
-                        tn.write("netstat -tlpn |grep :" + str(port) + "|awk -F \/ '{print $1}'|awk '{print $7}' > /tmp/tpid.out\n")
-                        tn.read_until(CMD_PROMPT, 3)
-                        tn.read_until(CMD_PROMPT, 3)
-                        tn.write("ps -hf -o cmd -p $(cat /tmp/tpid.out) \n")
-                        telnet_pid = tn.read_until("-F", 3)
-                        telnet_pid = telnet_pid.strip('\n')
-                        new_telnet = "/usr/bin/nohup " + telnet_pid + " -p " + str(new_port) + " >/dev/null 2>&1 &"
-                        print new_telnet.strip('\n')
-                        #tn.write("/usr/bin/nohup " + telnet_pid +
-                        print tn.read_until(CMD_PROMPT, 3)
+                start_telnet()
+                break
+            if case(5):
+                log.info("Create new random user, disable/nologin option.")
+                create_new_user(tn, user)
                 break
             if case(7):
                 log.info("Upload and run script on device option...")
                 upload_run_script()
                 break
             if case(999):
-                if (query_yes_no("Are you sure you want to kill telnet?", default="no")):
+                if (quiet or (query_yes_no("Are you sure you want to kill telnet?", default="no")) ):
                     log.warning("Killing telnet (verify alternate connectivity).")
+                    kill_telnet(tn, str(port))
                 break
             if case():
                 log.info("no case chosen")
