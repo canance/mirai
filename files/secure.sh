6d61a4156c8cd6b87c19ae11acbb54779fa430d9
Cory Nance <canance@coastal.edu>
2017-04-15 01:56:10 -0400

    Cleaned up secure.sh.

--- files/secure.sh
+++ files/secure.sh
@@ -3,33 +3,19 @@
 # serial:  14042017
 # Author: Cory Nance
 
-KILL="/bin/busybox kill"
 PS="/bin/busybox ps"
-SED="/bin/busybox sed"
-AWK="/bin/busybox awk"
 while true; do
-	
-	# check if there is a remote telnet connection
-	socket=$(grep /proc/net/tcp -e '[0-9]*: [A-Z0-9]*:[A-Z0-9]\{4\} [A-Z0-9]\{8\}:0017' | tr -s ' ' | cut -d' ' -f 11 || false)
+
+	socket=$(grep /proc/net/tcp -e '[0-9]*: [A-Z0-9]*:[A-Z0-9]\{4\} [A-Z0-9]\{8\}:0017' | tr -s ' ' | cut -d' ' -f 11)
 	if [ ! -z "$socket" ]; then
 		echo socket=$socket
 		master_pid=$(find /proc/ -type l 2>/dev/null | grep /fd/ | xargs ls -la 2>/dev/null | grep $socket | head -1 | tr -s ' ' | cut -f 9  -d ' ' | cut -f 3 -d '/')
 		echo master_pid=$master_pid
 		name=$($PS aux | grep $master_pid | head -1 | tr -s ' ' | cut -d ' ' -f 4)
-		$PS aux | grep $name | $SED \$d | $AWK '{print $1}' | xargs $KILL -9 2>/dev/null
-
-
-		# kill all child processes then kill master_pid
-		# $PKILL -TERM -P $master_pid
-
-
-		
-	else
-		echo 'nah'
+		$PS aux | grep $name | sed \$d | awk '{print $1}' | xargs kill -9 2>/dev/null
 	fi
 
-
-	sleep 1
+	sleep 10
 done
 
 
